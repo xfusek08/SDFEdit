88e89332890382e2255a1b198aed6c7b9a7a7311
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2023-01-24 13:56:41 +0100

    Update

--- resources/shaders/model_fill_brick.fs
+++ resources/shaders/model_fill_brick.fs
@@ -5,14 +5,9 @@ in mat4 brickTransformMatrix;
 flat in vec3  color;
 flat in float shininess;
 
-
 out vec4 fColor;
 
 void main() {
-    // fColor = vec4(vec3(shininess), 1);
-    
-    // fColor = vec4(fragPos.xyz, 1);
-    
     vec3 pos = (brickTransformMatrix * vec4(fragPos, 1)).xyz;
-    fColor = vec4(normalize(abs(pos)), 0.5);
+    fColor = vec4(normalize(abs(pos)), 1.0);
 }
